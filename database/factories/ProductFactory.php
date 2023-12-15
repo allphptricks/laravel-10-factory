f7dbfc41ea0c9799eb3f3aa92fe5930d874f7955
AllPHPTricks <allphptricks@gmail.com>
2023-12-15 21:08:24 +0500

    Initial Commit

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'code' => fake()->unique()->bothify('?????-#####'),
+            'name' => fake()->name(),
+            'quantity' => fake()->randomNumber(2, true),
+            'price' => fake()->randomFloat(2, 20, 90),
+            'description' => fake()->text(),
+        ];
+    }
+}
